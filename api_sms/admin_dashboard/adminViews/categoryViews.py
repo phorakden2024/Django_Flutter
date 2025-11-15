d2827f1f444245bd0ee0df893d8eea2526e07b9b
Panda <daphadenphorakden@gmail.com>
2025-11-15 22:18:03 +0700

    user

--- api_sms/admin_dashboard/adminViews/categoryViews.py
+++ api_sms/admin_dashboard/adminViews/categoryViews.py
@@ -6,4 +6,50 @@ import sweetify
 
 
 def category(request):
-          return render(request, 'setting/category/index.html')
+    category = Category.objects.all()
+    return render(request, 'setting/category/index.html', {'categories': category})
+
+def addform(request):
+    return render(request, 'setting/category/create.html')
+
+def store(request):
+    name = request.POST['name']
+    description = request.POST['description']
+    is_active = request.POST.get('is_active', False)
+    if is_active == 'false':
+        is_active = False
+    elif is_active == 'true':
+        is_active = True
+    else:
+        raise ValueError('["false" value must be either True or False.')
+    category = Category(name=name, description=description, is_active=is_active)
+    category.save()
+    sweetify.success(request, 'Category created successfully')
+    return redirect('/category/')
+def edit(request, id):
+    category = Category.objects.get(id=id)
+    return render(request, 'setting/category/edit.html', {'categories': category})
+
+def update(request, id):
+    if request.method == 'POST':
+        category = Category.objects.get(id=id)
+        category.name = request.POST['name']
+        category.description = request.POST['description']
+        is_active = request.POST.get('is_active', False)    
+        if is_active == 'false':
+            is_active = False
+        elif is_active == 'true':
+            is_active = True
+        else:
+            raise ValueError('["false" value must be either True or False.')
+        category.is_active = is_active
+        category.save()
+        sweetify.success(request, 'Category updated successfully')
+        return redirect('/category/')
+    return render(request, 'setting/category/edit.html')
+
+def delete(request, id):
+    category = Category.objects.get(id=id)
+    category.delete()
+    sweetify.success(request, 'Category deleted successfully')
+    return redirect('/category/')
